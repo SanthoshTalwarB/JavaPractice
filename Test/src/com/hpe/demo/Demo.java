4145e9ce1bf9619e3d26df0e5b51f3abcd15f2ce
bsanthos <bsanthos@FUCCXJJVHG.asiapacific.hpqcorp.net>
2018-11-15 12:39:46 +0530

    Added Demo class

---
+++ Test/src/com/hpe/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.hpe.demo;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Begin of main method");
+		System.out.println("End of main method");
+	}
+
+}
